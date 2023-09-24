9a0d39dc115f1d4938134f6014efc76b53b2921f
Jocn2020 <jonathannitisastro@gmail.com>
2023-09-24 13:25:34 -0700

    acm publications query

--- acm-researches/dblp_scraper.py
+++ acm-researches/dblp_scraper.py
@@ -1,11 +1,12 @@
 
 import requests
+import argparse
 from bs4 import BeautifulSoup
 import pandas as pd
+import csv
+import time
 
 from paper_search import semantic_scholar_search
-from mongodb.mongo_api import PublicationDB
-
 
 def crawl_dblp_author(dblp_url):
     response = requests.get(dblp_url)
@@ -20,7 +21,7 @@ def crawl_dblp_author(dblp_url):
     # Find the first paper listed on the profile page
     papers_list = soup.find_all('li', itemtype="http://schema.org/ScholarlyArticle")
     results = []
-    for paper in papers_list:
+    for i, paper in enumerate(papers_list):
         # get the data of the papers:
         title = paper.find('span', class_='title').text
         title = title[:-1] if title[-1] == '.' else title
@@ -38,21 +39,74 @@ def crawl_dblp_author(dblp_url):
             'abstract': ss_paper.abstract if (ss_paper and ss_paper.abstract) else '',
             'ss_id': ss_paper.ss_paper_id if ss_paper else '',
         })
+        if i%20 == 0:
+            time.sleep(1)
+
     return results
 
-if __name__ == "__main__":
-    acm_df = pd.read_csv('acm_csv/acm_turings.csv')
-    # test for one entry
-    first_author = acm_df.iloc[0]
-    
-    given_name = first_author['Given Name']
-    last_name = first_author['Last Name']
-    dblp_profile = first_author['DBLP profile']
+def list_of_dicts_to_csv(data, csv_path):
+    """
+    data: Dict[str, str] (non-nested dictionary)
+    file_path: str (path to csv file)
+    """
+    # Extract the keys from the first dictionary in the list
+    keys = data[0].keys()
 
-    if dblp_profile != "":
-        publications = crawl_dblp_author(dblp_profile)
-        publication_db = PublicationDB("acm_turings")
-        publication_db.insert_data([{
-            "name": f"{last_name}, {given_name}",
-            "publications": publications
-        }])
+    # Determine if the file exists or not
+    file_exists = False
+    try:
+        with open(csv_path, 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            file_exists = bool(next(reader, None))  # Check if the file is not empty
+    except FileNotFoundError:
+        pass
+
+    with open(csv_path, 'a', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=keys)
+
+        # If the file is empty, write the header row
+        if not file_exists:
+            writer.writeheader()
+
+        # Write each dictionary in the list as a row in the CSV file
+        for item in data:
+            writer.writerow(item)
+
+if __name__ == "__main__":
+    # load dataset
+    acm_turings_df = pd.read_csv('acm_csv/acm_turings.csv')
+    acm_fellows_df = pd.read_csv('acm_csv/acm_fellows.csv')
+
+    # Create the argument parser
+    parser = argparse.ArgumentParser(description='Search for authors in a DataFrame.')
+    # Add the author argument with the required format
+    parser.add_argument('author', help='Author in the format "Last Name, First Name".')
+    # Parse the command line arguments
+    args = parser.parse_args()
+
+
+    # Split the author argument into last_name and first_name
+    try:
+        last_name, first_name = args.author.split(', ')
+    except ValueError:
+        print('Invalid author format. Please use "Last Name, First Name".')
+        exit(1)
+    
+    # Search for matching rows in the DataFrame
+    fellows_matches = acm_fellows_df[(acm_fellows_df['Last Name'] == last_name) & (acm_fellows_df['Given Name'] == first_name)]
+    turings_matches = acm_turings_df[(acm_turings_df['Last Name'] == last_name) & (acm_turings_df['Given Name'] == first_name)]
+
+    # Display the matching rows
+    if not fellows_matches.empty or not turings_matches.empty:
+        dblp_profile = fellows_matches['DBLP profile'] if not fellows_matches.empty else turings_matches['DBLP profile']
+        dblp_profile = dblp_profile.values[0]
+        if dblp_profile != "":
+            publications = crawl_dblp_author(dblp_profile)
+            csv_file_path = f'acm_publications/acm_turings/{last_name}_{first_name}_publications.csv'
+            list_of_dicts_to_csv(publications, csv_file_path)
+        else:
+            print(f'Author {args.author} DBLP profile is not found, please update the ACM csv file.')
+    else:
+        print('No ACM authors found.')
+
+    
